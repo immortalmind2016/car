c64401ea6b5d63370b338ea448fdbbafc9627369
Mohamed Salah <immortal.mind2016@gmail.com>
2022-05-29 01:14:40 +0200

    refactor: class interface

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -9,7 +9,11 @@ import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
 import { UserDto } from 'src/users/dtos/user.dto';
 
-export function Serialize(dto: any) {
+//Interface for any class
+interface ClassConstructor {
+  new (...args: any[]): object;
+}
+export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 export class SerializeInterceptor implements NestInterceptor {
